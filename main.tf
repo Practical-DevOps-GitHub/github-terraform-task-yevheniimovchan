d5c9ecd1c156f6423cf00d107c29fc55b32c8578
yevheniimovchan <127136678+yevheniimovchan@users.noreply.github.com>
2023-08-03 14:06:15 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,3 @@
-terraform {
-  required_providers {
-    github = {
-      source  = "integrations/github"
-      version = "~> 5.0"
-    }
-  }
-}
-
-# Configure the GitHub Provider
-
 provider "github" {
   token = var.token
   owner = "Practical-DevOps-GitHub"
